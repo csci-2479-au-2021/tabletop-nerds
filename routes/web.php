a90095cdc88a528c498c05a09f42087819b84f6c
Colin Truong <ctruong@student.cscc.edu>
2021-09-29 19:37:29 -0400

    Colin Homework2

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/profile', function () {
+    return view('profile');
+})->middleware(['auth'])->name('profile');
 
 require __DIR__.'/auth.php';
