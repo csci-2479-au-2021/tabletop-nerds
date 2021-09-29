5389f8f3f92cf9a7f910978a359e202f822874a5
Andrew Harris <harris.1305@gmail.com>
2021-09-29 19:47:12 -0400

    hw 2 example solution

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/games', [GameController::class, 'listGames']);
+// ->name('Games');
 
 require __DIR__.'/auth.php';
