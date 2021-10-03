e8b022880fc19dab929466d1a73c624a73b317ba
Sarah Sheets <ssheets5@student.cscc.edu>
2021-10-03 18:04:01 -0400

    #25 incomplete

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/phpinfo', function () {
-    return phpinfo();
-});
+Route::get('/games', [GameController::class, 'listGames']);
 
 require __DIR__.'/auth.php';
