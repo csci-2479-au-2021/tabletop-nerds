3da40d4d65058a0f5d76ba7bca9dddd10ab7b7b5
Sarah Sheets <ssheets5@student.cscc.edu>
2021-10-04 13:24:05 -0400

    #25 game list regurgitation

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -9,11 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <!--game list-->
-                    <ul>My Games:</ul>
-                    @foreach ($games as $game)
-                        <li>{{$game}}</li>
-                    @endforeach
+                    My Games:
+                    <ul>
+                        @foreach($games as $game)
+                            <li>{{$game}}</li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
