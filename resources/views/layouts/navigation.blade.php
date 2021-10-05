63331e29210c726779f90bca070190675281a841
Daniel Shea <80367833+dshea18@users.noreply.github.com>
2021-10-05 12:14:50 -0400

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -78,9 +78,7 @@
             <x-responsive-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
                 {{ __('Account Profile') }}
             </x-responsive-nav-link>
-        </div>
-        
-        <div class="pt-2 pb-3 space-y-1">
+       
             <x-responsive-nav-link :href="route('games')" :active="request()->routeIs('games')">
                 {{ __('Games') }}
             </x-responsive-nav-link>
