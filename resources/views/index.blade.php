41704d9dd035f02da4b9eede716d669780a41b12
Sarah Sheets <ssheets5@student.cscc.edu>
2021-10-07 04:42:16 -0400

    lookingoodlayout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 style="text-align:center;" class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Account Profile') }}
+            {{ __('Index/Home/Not Laravel') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div style="text-align:center;"class="p-6 bg-white border-b border-gray-200">
-                    Acct details
+                    Anything other than the horrible splash landing Andrew hates.
                 </div>
             </div>
         </div>
