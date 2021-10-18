39595d0d2e5043dd50b8f9ffff986be80d513cfe
Colin <80017745+ctruong1@users.noreply.github.com>
2021-10-18 11:29:52 -0400

    UserService

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories; 
+
+class UserRepository
+{
+    public function getWishlistByUserId(): array
+    {
+        $wishlist = [];
+
+        return $wishlist;
+    }
+}
---
+++ app/Services/UserService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository; 
+
+class UserService 
+{
+    public function __construct(
+       //private UserRepository $userRepository
+    ) {}
+
+    public function getWishlistByUserId(): array
+    {
+        return $this->userRepository->getWishlistByUserId();
+    }
+}
