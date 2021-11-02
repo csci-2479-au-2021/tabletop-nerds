07f27a0057b3a29a75c864fb1a056f1f2ef9135b
dshea18 <dshea2@student.cscc.edu>
2021-11-01 20:03:00 -0400

    updated game-service

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -10,10 +10,13 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-
-    public function __construct(
-        private GameService $gameService
-        ) { }
+    private GameService $gameService;
+    
+    public function __construct(GameService $gameService)
+    {
+        $this->gameService = $gameService;
+    }
+        
 
     public function index()
     {
@@ -24,20 +27,14 @@ class GameController extends Controller
         ]);
     }
 
-    // this function needs updated to find id from game DB + disp individual game details
+    public function listGames(){
+        return view('games', ['games'=>$this->gameService->getGames()]);
+    }
+
+    
     
     public function gameView($id){
-        $monopoly = new Game([
-            'title' => "Monopoly",
-            'description' => "Monopoly is a game with a bad reputation that it doesn't deserve. If players read the rules and played this game utilizing auctions,
-        the complaint that this game takes too long would dissapear. Read and play by the rules. Fight me if you disagree.",
-        "http://localhost/images/monopoly.jpg"]);
-        $sorry = new Game([
-            'title' => "Sorry",
-            'description' => "I actually don't know anything about this game. The image was easy to find though. LOL, Sorry.",
-        "http://localhost/images/sorry.jpg"]);
-        $gamelist = [$monopoly, $sorry];   
-        $game = $gamelist[$id];
-        return view('gameView', compact('game')); 
+        return view('gameView', ['game'=>$this->gameService->getGameById($id)]);
+
     }
 }
--- app/Services/GameService.php
+++ app/Services/GameService.php
@@ -4,15 +4,45 @@ namespace App\Services;
 
 use App\Repositories\GameRepository;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use App\Models\Game;
 
 class GameService
 {
-    public function __construct(
-        private GameRepository $gameRepository
-    ) {}
 
-    public function getGames(): Collection
-    {
-        return $this->gameRepository->getGames();
+    public function __construct(GameRepository $gameRepository)
+        {
+            $this->gameRepository = $gameRepository;
+        }
+    
+    public function getGameById($id){
+        $monopoly = new Game([
+            'title' => "Monopoly",
+            'description' => "Monopoly is a game with a bad reputation that it doesn't deserve. If players read the rules and played this game utilizing auctions,
+                                the complaint that this game takes too long would dissapear. Read and play by the rules. Fight me if you disagree.",
+            'image' =>"http://localhost/images/monopoly.jpg"]);
+        $sorry = new Game([
+            'title' => "Sorry",
+            'description' => "I actually don't know anything about this game. The image was easy to find though. LOL, Sorry.",
+            'image' =>"http://localhost/images/sorry.jpg"]);
+        $gamelist = [$monopoly, $sorry];   
+        $game = $gamelist[$id];
+        // When respository has a method to get game by ID uncomment and insert into line below.
+        // $game = $this->gameRepository->[insertmethodrepositorymetherehere]($id);
+        return($game);
     }
+
+    public function getGames($orderby = null, $direction = null, $limit= null)
+    {   
+        return ($this->gameRepository->getGames());
+    }
+
+    
+
+    public function searchGamesByTitle (Request $title)
+    {    
+        return ($this->gameRepository->searchGamesByTitle($title));
+    }
+
+
 }
