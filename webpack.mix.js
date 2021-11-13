67c86e065fdf787f83920a05a0d86c354da9dc94
Andrew Harris <harris.1305@gmail.com>
2021-11-13 14:11:18 -0500

    add typescript boilerplate code to our app

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-]);
+mix.ts('resources/app/app.ts', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]).sourceMaps();
